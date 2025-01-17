30a9d02e520da4c137ebf30b1e5c04301520917b
JonathanG <thatguyjono94@gmail.com>
2025-01-18 00:06:53 +1300

    merge sorted singly linked list

--- dsa/leetcode/21/solution.js
+++ dsa/leetcode/21/solution.js
@@ -11,5 +11,21 @@
  * @return {ListNode}
  */
 const mergeTwoLists = (list1, list2) => {
+    const head = new ListNode(-1)
+    let list = head
 
+    while (list1 && list2) {
+        if (list1.val > list2.val) {
+            list.next = list2
+            list2 = list2.next
+        } else {
+            list.next = list1
+            list1 = list1.next
+        }
+        list = list.next
+    }
+
+    list.next = list1 || list2
+
+    return head.next
 }
