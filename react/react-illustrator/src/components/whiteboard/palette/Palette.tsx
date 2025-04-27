afd8af824111e70f8ef5fcce0da86b4b89bf6e34
JonathanG <thatguyjono94@gmail.com>
2025-04-28 03:18:22 +1200

    IP

--- react/react-illustrator/src/components/whiteboard/palette/Palette.tsx
+++ react/react-illustrator/src/components/whiteboard/palette/Palette.tsx
@@ -1,21 +1,22 @@
 import React from 'react'
+import paletteStyle from '@src/styles/whiteboard/palette/Pallete.module.css'
 
 const Palette: React.FC  = () => {
     return (
         <div>
             <div>
-                <span>Stroke</span>
+                <span className={paletteStyle.label}>Stroke</span>
             </div>
-            <div>
-                <div>
-                    <div className="black"></div>
-                    <div className="red"></div>
-                    <div className="green"></div>
-                    <div className="blue"></div>
-                    <div className="orange"></div>
+            <div className={paletteStyle.colors}>
+                <div className={paletteStyle.colorRange}>
+                    <div className={`${paletteStyle.black} ${paletteStyle.colorBlock}`}></div>
+                    <div className={`${paletteStyle.red} ${paletteStyle.colorBlock}`}></div>
+                    <div className={`${paletteStyle.green} ${paletteStyle.colorBlock}`}></div>
+                    <div className={`${paletteStyle.blue} ${paletteStyle.colorBlock}`}></div>
+                    <div className={`${paletteStyle.orange} ${paletteStyle.colorBlock}`}></div>
                 </div>
                 <div>
-
+                    <div className={paletteStyle.colorPicker}></div>
                 </div>
             </div>
         </div>
