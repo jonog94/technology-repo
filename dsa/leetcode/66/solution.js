5e9e0338d64c500cbcf4a8b3cd24403a34d5a634
JonathanG <thatguyjono94@gmail.com>
2025-07-27 21:50:54 +1200

    leetcode 66 JS

--- dsa/leetcode/66/solution.js
+++ dsa/leetcode/66/solution.js
@@ -3,5 +3,26 @@
  * @return {number[]}
  */
 const plusOne = (digits) => {
+    const arr = []
 
+    let add = true
+
+    for (let i = digits.length - 1; i >= 0; i--) {
+        const n = digits[i]
+
+        if (add && (n + 1) > 9) {
+            arr.unshift(0)
+
+            if (i === 0) {
+                arr.unshift(1)
+            }
+        } else if (add) {
+            arr.unshift(n + 1)
+            add = false
+        } else {
+            arr.unshift(n)
+        }
+    }
+
+    return arr
 }
